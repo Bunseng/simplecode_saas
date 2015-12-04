fff62e8a8e2feb0a205537d13e2242b480158629
Bunrith Seng <seng4510@gmail.com>
2015-12-04 22:25:46 +0000

    Added validation and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -20,7 +20,6 @@ class ContactsController < ApplicationController
         end
             
     end
-    
     private
     def contact_params
         params.require(:contact).permit(:name, :email, :comments)
